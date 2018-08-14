43428e64f7d802fa154e1c593c1f3c59b8008df6
Jeff Musa <jeffmusa@gmail.com>
2018-08-14 09:13:02 +0300

    restructured gh-pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
 
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
@@ -8,6 +11,7 @@ import { GoalDetailsComponent } from './goal-details/goal-details.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
+import {AlertsService} from './alert-service/alerts.service';
 
 
 @NgModule({
@@ -21,9 +25,12 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
   ],
   imports: [
      BrowserModule,
-     FormsModule
+     FormsModule,
+     HttpClientModule,
+     NgProgressModule.forRoot(),// normal progress bar
+     NgProgressHttpModule // progress bar to load http requests
    ],
-  providers: [],
+  providers: [AlertsService], // Add service to providers
   bootstrap: [AppComponent]
 })
 export class AppModule { }
